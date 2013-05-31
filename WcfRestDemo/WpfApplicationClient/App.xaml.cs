27b02924bbc6dc489b43f149512997be342c7477
久永 <Jiuyong@hotmail.com>
2013-05-31 22:42:27 +0800

    修正项目的默认命名空间和程序集名称。

--- WcfRestDemo/WpfApplicationClient/App.xaml.cs
+++ WcfRestDemo/WpfApplicationClient/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace WpfApplicationClient
+namespace Jiuyong
 {
 	/// <summary>
 	/// App.xaml 的交互逻辑
--- WcfRestDemo/WpfApplicationClient/MainWindow.xaml.cs
+++ WcfRestDemo/WpfApplicationClient/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplicationClient
+namespace Jiuyong
 {
 	/// <summary>
 	/// MainWindow.xaml 的交互逻辑
