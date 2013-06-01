031012b2d0b80a4f8d9c0373443b67c534b0e6e8
久永 <Jiuyong@hotmail.com>
2013-06-01 17:26:16 +0800

    按 “可批量处理“替换原则来命名项目结构。添加 WPF 客户端项目。

--- Demo/DemoWpfClient/Properties/AssemblyInfo.cs
+++ Demo/DemoWpfClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Jiuyong")]
+[assembly: AssemblyTitle("DemoWpfClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jiuyong")]
+[assembly: AssemblyProduct("DemoWpfClient")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Demo/DemoWpfClient/Properties/Resources.Designer.cs
+++ Demo/DemoWpfClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Jiuyong.Properties {
+namespace Demo.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Jiuyong.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Jiuyong.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Demo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
